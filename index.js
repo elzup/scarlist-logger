3c79428cab2bee5c86eee660d474ed7b399c3923
elzup <guild0105@gmail.com>
2018-11-07 18:23:33 +0900

    Initialize commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const arpScanner = require("arpscan/promise");
+
+const token = process.env.SCARLIST_TOKEN;
+const interface = process.env.SCARLIST_INTERFACE || "wlan0";
+const host = process.env.SCARLIST_HOST;
+
+axios.defaults.baseURL = host;
+axios.defaults.headers.common["Authorization"] = token;
+
+(async () => {
+  const data = await arpScanner({ interface });
+  const registeredMa = Object.keys((await axios.get("/mac_addrs")).data);
+
+  const liveMa = data.map(v => v.mac.toLowerCase());
+  const macAddrs = liveMa.filter(ma => ma in registeredMa);
+  console.log({ macAddrs, liveMa, registeredMa });
+  //scarlist.anozon.me/mac_addrs?room_id=planck_units
+  await axios.post("/log", {
+    room_id: "cps",
+    mac_addrs: macAddrs
+  });
+})();
